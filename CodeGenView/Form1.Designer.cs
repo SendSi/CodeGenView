c58a83f1c03f0f81c26374934c1fc943d4b9e682
曾思信 <389585111@qq.com>
2025-03-19 17:39:26 +0800

    测试

---
+++ CodeGenView/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CodeGenView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnGenView = new System.Windows.Forms.Button();
+            this.inputViewTxt = new System.Windows.Forms.TextBox();
+            this.genTargetTxt = new System.Windows.Forms.RichTextBox();
+            this.btnGenViewDesktop = new System.Windows.Forms.Button();
+            this.globalTip = new System.Windows.Forms.ToolTip(this.components);
+            this.inputMgrTxt = new System.Windows.Forms.TextBox();
+            this.btnGenMgr = new System.Windows.Forms.Button();
+            this.btnGenMgrDesktop = new System.Windows.Forms.Button();
+            this.btnGenAllDesktop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenView
+            // 
+            this.btnGenView.Location = new System.Drawing.Point(315, 12);
+            this.btnGenView.Name = "btnGenView";
+            this.btnGenView.Size = new System.Drawing.Size(80, 23);
+            this.btnGenView.TabIndex = 0;
+            this.btnGenView.Text = "查看字符串";
+            this.btnGenView.UseVisualStyleBackColor = true;
+            this.btnGenView.Click += new System.EventHandler(this.btnGenView_Click);
+            // 
+            // inputViewTxt
+            // 
+            this.inputViewTxt.Location = new System.Drawing.Point(18, 14);
+            this.inputViewTxt.Name = "inputViewTxt";
+            this.inputViewTxt.Size = new System.Drawing.Size(291, 21);
+            this.inputViewTxt.TabIndex = 1;
+            this.inputViewTxt.Text = "League;LeagueMainView";
+            // 
+            // genTargetTxt
+            // 
+            this.genTargetTxt.Location = new System.Drawing.Point(486, 12);
+            this.genTargetTxt.Name = "genTargetTxt";
+            this.genTargetTxt.Size = new System.Drawing.Size(632, 718);
+            this.genTargetTxt.TabIndex = 2;
+            this.genTargetTxt.Text = "";
+            // 
+            // btnGenViewDesktop
+            // 
+            this.btnGenViewDesktop.Location = new System.Drawing.Point(401, 12);
+            this.btnGenViewDesktop.Name = "btnGenViewDesktop";
+            this.btnGenViewDesktop.Size = new System.Drawing.Size(75, 23);
+            this.btnGenViewDesktop.TabIndex = 3;
+            this.btnGenViewDesktop.Text = "生成在桌面";
+            this.btnGenViewDesktop.UseVisualStyleBackColor = true;
+            this.btnGenViewDesktop.Click += new System.EventHandler(this.btnGenViewDesktop_Click);
+            // 
+            // inputMgrTxt
+            // 
+            this.inputMgrTxt.Location = new System.Drawing.Point(18, 51);
+            this.inputMgrTxt.Name = "inputMgrTxt";
+            this.inputMgrTxt.Size = new System.Drawing.Size(291, 21);
+            this.inputMgrTxt.TabIndex = 4;
+            this.inputMgrTxt.Text = "League";
+            // 
+            // btnGenMgr
+            // 
+            this.btnGenMgr.Location = new System.Drawing.Point(315, 51);
+            this.btnGenMgr.Name = "btnGenMgr";
+            this.btnGenMgr.Size = new System.Drawing.Size(80, 23);
+            this.btnGenMgr.TabIndex = 5;
+            this.btnGenMgr.Text = "查看字符串";
+            this.btnGenMgr.UseVisualStyleBackColor = true;
+            this.btnGenMgr.Click += new System.EventHandler(this.btnGenMgr_Click);
+            // 
+            // btnGenMgrDesktop
+            // 
+            this.btnGenMgrDesktop.Location = new System.Drawing.Point(405, 51);
+            this.btnGenMgrDesktop.Name = "btnGenMgrDesktop";
+            this.btnGenMgrDesktop.Size = new System.Drawing.Size(75, 23);
+            this.btnGenMgrDesktop.TabIndex = 6;
+            this.btnGenMgrDesktop.Text = "生成在桌面";
+            this.btnGenMgrDesktop.UseVisualStyleBackColor = true;
+            this.btnGenMgrDesktop.Click += new System.EventHandler(this.btnGenMgrDesktop_Click);
+            // 
+            // btnGenAllDesktop
+            // 
+            this.btnGenAllDesktop.Font = new System.Drawing.Font("宋体", 13F);
+            this.btnGenAllDesktop.Location = new System.Drawing.Point(315, 687);
+            this.btnGenAllDesktop.Name = "btnGenAllDesktop";
+            this.btnGenAllDesktop.Size = new System.Drawing.Size(161, 43);
+            this.btnGenAllDesktop.TabIndex = 7;
+            this.btnGenAllDesktop.Text = "全_生成在桌面";
+            this.btnGenAllDesktop.UseVisualStyleBackColor = true;
+            this.btnGenAllDesktop.Click += new System.EventHandler(this.btnGenAllDesktop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 742);
+            this.Controls.Add(this.btnGenAllDesktop);
+            this.Controls.Add(this.btnGenMgrDesktop);
+            this.Controls.Add(this.btnGenMgr);
+            this.Controls.Add(this.inputMgrTxt);
+            this.Controls.Add(this.btnGenViewDesktop);
+            this.Controls.Add(this.genTargetTxt);
+            this.Controls.Add(this.inputViewTxt);
+            this.Controls.Add(this.btnGenView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Button btnGenView;
+        private System.Windows.Forms.TextBox inputViewTxt;
+        private System.Windows.Forms.RichTextBox genTargetTxt;
+        private System.Windows.Forms.Button btnGenViewDesktop;
+        private System.Windows.Forms.ToolTip globalTip;
+        private System.Windows.Forms.TextBox inputMgrTxt;
+        private System.Windows.Forms.Button btnGenMgr;
+        private System.Windows.Forms.Button btnGenMgrDesktop;
+        private System.Windows.Forms.Button btnGenAllDesktop;
+    }
+}
---
+++ CodeGenView/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CodeGenView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetProgramDirectory()
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo directory = new DirectoryInfo(baseDirectory);
+            while (directory.Name == "Debug" || directory.Name == "Release")
+            {
+                directory = directory.Parent;
+            }
+            while (directory.Name == "bin")
+            {
+                directory = directory.Parent;
+            }
+            string projectRootDirectory = directory.FullName;
+            return projectRootDirectory;
+        }
+
+        private void btnGenView_Click(object sender, EventArgs e)
+        {
+            GenTxt(inputViewTxt.Text, "codeView.txt");
+        }
+
+        void GenTxt(string input, string tmpName)
+        {
+            var proPath = GetProgramDirectory();
+            string filePath = Path.Combine(proPath, tmpName);
+            if (File.Exists(filePath))
+            {
+                string uiViewStr = File.ReadAllText(filePath);
+                var maxIndex = GetMaxNumParam(uiViewStr); //匹配 得到  含{0} {1} {2} ... 最大的数值 是多少
+                var inputTxts = input.Split(';').ToList();
+
+                while (inputTxts.Count <= maxIndex)
+                {
+                    inputTxts.Add(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")); // 使用空字符串补全
+                }
+                var str = string.Format(uiViewStr, inputTxts.ToArray());
+                genTargetTxt.Text = str;
+            }
+        }
+
+
+        int GetMaxNumParam(string strContent)
+        {
+            // 匹配形如 {x} 的占位符，x 是数字
+            Regex placeholderRegex = new Regex(@"\{(\d+)\}");
+            var matches = placeholderRegex.Matches(strContent);
+            int maxPlaceholder = -1;
+            foreach (Match match in matches)
+            {
+                if (int.TryParse(match.Groups[1].Value, out int value))
+                {
+                    if (value > maxPlaceholder)
+                    {
+                        maxPlaceholder = value;
+                    }
+                }
+            }
+            // 输出结果
+            Console.WriteLine($"最大占位符中的数值是: {maxPlaceholder}");
+            return maxPlaceholder;
+        }
+
+        private void btnGenViewDesktop_Click(object sender, EventArgs e)
+        {  //把 字符串 生成在 桌面上  **.lua文件 
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            var inputTxts = inputViewTxt.Text.Split(';');
+            string fileName = inputTxts[inputTxts.Length - 1];
+            string filePath = Path.Combine(desktopPath, $"{fileName}.lua");
+            btnGenView_Click(null, null);
+            File.WriteAllText(filePath, genTargetTxt.Text, Encoding.UTF8);//使用UTF-8编码保存文件
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            globalTip.AutoPopDelay = 25000;
+            globalTip.InitialDelay = 100;
+            globalTip.ReshowDelay = 0;
+            globalTip.ShowAlways = true;
+            globalTip.SetToolTip(this.inputViewTxt, "匹配codeView.txt文件_查看占用的{0}{1}{2}...使用分号;隔开多个参数");
+            globalTip.SetToolTip(this.inputMgrTxt, "匹配codeMgr.txt文件_查看占用的{0}{1}{2}...使用分号;隔开多个参数");
+        }
+
+        private void btnGenMgrDesktop_Click(object sender, EventArgs e)
+        {
+            //把 字符串 生成在 桌面上  **.lua文件 
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            var inputTxts = inputMgrTxt.Text.Split(';');
+            string fileName = inputTxts[0];
+            string filePath = Path.Combine(desktopPath, $"{fileName}.lua");
+            btnGenMgr_Click(null, null);
+            File.WriteAllText(filePath, genTargetTxt.Text, Encoding.UTF8);//使用UTF-8编码保存文件
+        }
+
+        private void btnGenMgr_Click(object sender, EventArgs e)
+        {
+            GenTxt(inputMgrTxt.Text, "codeMgr.txt");
+        }
+
+        private void btnGenAllDesktop_Click(object sender, EventArgs e)
+        {
+            btnGenMgrDesktop_Click(null, null);
+            btnGenViewDesktop_Click(null, null);
+        }
+    }
+}
